49e827f39f71ea03b6e6a76a645e7f78aba03aa1
Eduardo Robles <EngEduardoRobles@outlook.com>
2024-12-12 04:13:15 -0600

    feat: integrate MongoDB with Docker and Node.js

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+
+
+// middleware
+
+
+
+
+console.log(process.env.MONGO_URI)
+//mongo db connection 
+mongoose.connect(process.env.MONGO_URI, {})
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err))
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`erver running on port ${PORT}`))
