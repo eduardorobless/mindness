383ab00757aee25decb5bbf8a921aee248891837
Eduardo Robles <EngEduardoRobles@outlook.com>
2024-12-15 02:35:44 -0600

    editing functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ function App() {
     const [loading, setLoading] = useState(true)
 
 
+    const [editingRow, setEditingRow] = useState(null)
+    const [editedRowData, setEditedRowData] = useState({ title: '', content: '' })
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -25,6 +28,56 @@ function App() {
         fetchData();
 
     }, [])
+
+
+    const handleEdit = (note) => {
+        console.log('editing: ', note._id)
+
+        setEditingRow(note._id)
+        setEditedRowData({ title: note.title, content: note.content })
+    };
+
+    const handleChange = (e) => {
+        setEditedRowData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+
+    const handleConfirm = async () => {
+        try {
+            const response = await fetch(`http://[::1]:5000/api/${editingRow}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(editedRowData)
+            })
+
+            if (response.ok) {
+
+                setTableData((prevData) =>
+                    prevData.map((note) =>
+                        note._id == editingRow ? { ...note, ...editedRowData } : note
+                    ))
+
+                resetFields()
+
+            } else {
+                console.error('Failed to update data')
+            }
+        } catch (error) {
+            console.error('Error updating data: ', error)
+        }
+    }
+
+    const handleCancel = () => {
+        resetFields()
+    }
+
+    const resetFields = () => {
+        setEditingRow(null)
+        setEditedRowData({ title: '', content: '' })
+    }
+
+
 
 
     if (loading) {
@@ -44,8 +97,48 @@ function App() {
                 <tbody>
                     {tableData.map((note) => (
                         <tr key={note._id}>
-                            <td>{note.title}</td>
-                            <td>{note.content}</td>
+                            <td>
+
+                                {editingRow === note._id ? (
+                                    <input
+                                        type="text"
+                                        name="title"
+                                        value={editedRowData.title}
+                                        onChange={handleChange}
+                                    />
+                                ) : (
+                                    note.title
+                                )
+                                }
+                            </td>
+
+                            <td>
+                                {
+                                    editingRow === note._id ? (
+                                        <input
+                                            type="text"
+                                            name="content"
+                                            value={editedRowData.content}
+                                            onChange={handleChange}
+                                        />
+                                    ) : (
+                                        note.content
+                                    )
+                                }
+
+                            </td>
+                            <td>
+                                {
+                                    editingRow === note._id ? (
+                                        <>
+                                            <button onClick={handleConfirm}>Confirm</button>
+                                            <button onClick={handleCancel}>Cancel</button>
+                                        </>
+                                    ) : (
+                                        <button onClick={() => handleEdit(note)} >Edit</button>
+                                    )
+                                }
+                            </td>
                         </tr>
                     ))}
                 </tbody>
